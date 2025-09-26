39f59d605ebf6c1bdd571a765e88acd8f47b8b76
unknown <clappertontee@gmail.com>
2025-09-26 10:19:29 +0200

    I have add the pagination component

---
+++ my-app/src/utilities/Pagination.jsx
@@ -0,0 +1,18 @@
+import react from "react";
+
+export default function Pagination({ page, totalPages, onPageChange }) {
+	if (totalPages <= 1) return null;
+	return (
+		<div className="pagination">
+			<button disabled={page === 1} onClick={() => onPageChange(page - 1)}>
+				Prev
+			</button>
+			<span>
+				Page {page}/{totalPages}
+			</span>
+			<button disabled={page === totalPages} onClick={() => onPageChange + 1}>
+				Next
+			</button>
+		</div>
+	);
+}
