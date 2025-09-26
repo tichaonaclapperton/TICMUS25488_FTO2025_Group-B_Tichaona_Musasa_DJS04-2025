e5a19e478e5a82058ce55a3f9a46a42c6e29840d
unknown <clappertontee@gmail.com>
2025-09-26 09:56:02 +0200

    I have handled sorting dropdown

---
+++ my-app/src/utilities/GenreFilter.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+import {genres} from "../genres/data.js";
+
+export default function GenreFilter({value, onChange}){
+    return(
+        <select value={value} onChange={(e) => onChange(e.target.value)}>
+            <option value="">All genres</option>
+            {genres.map((g) =>(
+                <option key={g.id} value={g.title}>
+                    {g.title}
+                </option>
+            ))}
+        </select>
+
+    );
+}
