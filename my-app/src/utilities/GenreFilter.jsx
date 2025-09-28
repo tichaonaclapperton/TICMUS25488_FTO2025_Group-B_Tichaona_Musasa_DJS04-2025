f2303c901bd6f0e18594583420a7a45446511e79
unknown <clappertontee@gmail.com>
2025-09-28 10:59:41 +0200

    I have commented using jsdoc

--- my-app/src/utilities/GenreFilter.jsx
+++ my-app/src/utilities/GenreFilter.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import {genres} from "../genres/data.js";
+import { genres } from "../genres/data.js";
 
-export default function GenreFilter({value, onChange}){
-    return(
-        <select value={value} onChange={(e) => onChange(e.target.value)}>
-            <option value="">All genres</option>
-            {genres.map((g) =>(
-                <option key={g.id} value={g.title}>
-                    {g.title}
-                </option>
-            ))}
-        </select>
+/**
+ * Dropdown component for filtering podcasts by genre.
+ *
+ * @component
+ * @param {Object} props
+ * @param {string} props.value - The currently selected genre title.
+ * @param {Function} props.onChange - Callback fired when the genre changes.
+ * @returns {JSX.Element} A genre filter dropdown.
+ */
 
-    );
+export default function GenreFilter({ value, onChange }) {
+	return (
+		<select value={value} onChange={(e) => onChange(e.target.value)}>
+			<option value="">All genres</option>
+			{genres.map((g) => (
+				<option key={g.id} value={g.title}>
+					{g.title}
+				</option>
+			))}
+		</select>
+	);
 }
